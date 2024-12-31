cd3e745b44280d5effced88cd20e53bbe8f0a4a6
sadrian94 <sadrian94@gmail.com>
2024-12-31 16:44:50 -0600

    Step 3: Model complete ml/model.py complete functions train_model, inference, save_model, load_model, performance_on_categorical_slice

--- ml/model.py
+++ ml/model.py
@@ -1,4 +1,6 @@
 import pickle
+
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
@@ -19,8 +21,9 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    # TODO: implement the function
-    pass
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -59,8 +62,8 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
+    preds = model.predict(X)
+    return (preds > 0.5).astype(int)
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -72,13 +75,13 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
+    with open(path, 'wb') as file:
+        pickle.dump(model, file)
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
+    with open(path, 'rb') as file:
+        return pickle.load(file)
 
 
 def performance_on_categorical_slice(
@@ -103,7 +106,7 @@ def performance_on_categorical_slice(
     label : str
         Name of the label column in `X`. If None, then an empty array will be returned
         for y (default=None)
-    encoder : sklearn.preprocessing._encoders.OneHotEncoder
+    encoder : sklearn.preprocessing._encoders.OneHortEncoder
         Trained sklearn OneHotEncoder, only used if training=False.
     lb : sklearn.preprocessing._label.LabelBinarizer
         Trained sklearn LabelBinarizer, only used if training=False.
@@ -117,12 +120,18 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    # TODO: implement the function
+    slice_data = data[data[column_name] == slice_value]
     X_slice, y_slice, _, _ = process_data(
         # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
+        # for input data, use data in column given as "column_name", with the slice_value
         # use training = False
+        slice_data,
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice) # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
